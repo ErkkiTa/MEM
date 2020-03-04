f4b557b6fe15e7b4677516cb4b6c529d10ed30f0
erkileemet <55434235+erkileemet@users.noreply.github.com>
2020-03-04 14:50:02 +0200

    Update and rename app.js to api.js

---
+++ api.js
@@ -0,0 +1,34 @@
+fetch('http://andmebaas.stat.ee/sdmx-json/data/PA633/DBL113+DBL114+DBL245+DBL246+DBL248+DBL251+DBL252+DBL258+DBL272+DBL273+DBL285+DBL297+DBL386+DBL401+DBL410.1+2+3.1+2+3/all?startTime=2010&endTime=2014&dimensionAtObservation=allDimensions')
+.then(response => {
+    return response.json()
+}).then(data => {
+    console.log(data)
+    const options = document.getElementById("options")
+
+    data.structure.dimensions.observation[0].values.forEach(test_value => {
+        console.log(test_value)
+        let testDiv = document.createElement("div")
+        testDiv.innerHTML = '<div>' + test_value.name + '</div>'
+        testDiv.classList.add("test")
+        option1.append(testDiv)
+    
+    });
+
+    data.structure.dimensions.observation[1].values.forEach(test_value => {
+        console.log(test_value)
+        let testDiv = document.createElement("div")
+        testDiv.innerHTML = '<div>' + test_value.name + '</div>'
+        testDiv.classList.add("test")
+        option1.append(testDiv)
+    
+    });
+
+    data.structure.dimensions.observation[2].values.forEach(test_value => {
+        console.log(test_value)
+        let testDiv = document.createElement("div")
+        testDiv.innerHTML = '<div>' + test_value.name + '</div>'
+        testDiv.classList.add("test")
+        option1.append(testDiv)
+    
+    });
+
