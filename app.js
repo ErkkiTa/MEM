326ed7046d85349464629918588619a4fb79735a
ratsepmarkus <56824783+ratsepmarkus@users.noreply.github.com>
2020-03-05 13:26:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,73 @@
-function getInputValue(){
+// feedback
+document.querySelectorAll('.feedback li').forEach(entry => entry.addEventListener('click', e => {
+    if (!entry.classList.contains('active')) {
+        document.querySelector('.feedback li.active').classList.remove('active');
+        entry.classList.add('active');
+    }
+    e.preventDefault();
+}));
 
-  var moneyarray = [0, 5.07, 3.80, 5.58, 3.81, 5.78, 3.80, 6.73, 6.04, 5.31, 3.99, 3.77, 4.48, 3.95]
+function showBottom(value) {
+    document.getElementById("occupation").value = value;
+}
 
-  var inputValue= document.getElementById("hours").value;
-  var amet = document.getElementById("occupation").value;
+//arvutus
 
-  if (amet == 'kokk') {
-      multiply = inputValue * moneyarray[1];
-  }
-  if (amet == 'õpetaja') {
-      multiply = inputValue * moneyarray[2];
-  }
-  if (amet == 'autojuht') {
-      multiply = inputValue * moneyarray[3];
-  }
-  if (amet == 'lapsehoidja') {
-      multiply = inputValue * moneyarray[4];
-  }
-  if (amet == 'teenija') {
-      multiply = inputValue * moneyarray[5];
-  }
-  if (amet == 'majahoidja') {
-      multiply = inputValue * moneyarray[6];
-  }
-  if (amet == 'aednik') {
-      multiply = inputValue * moneyarray[7];
-  }
-  if (amet == 'loomakasvataja') {
-      multiply = inputValue * moneyarray[8];
-  }
-  if (amet == 'õmbleja') {
-      multiply = inputValue * moneyarray[9];
-  }
-  if (amet == 'juuksur') {
-      multiply = inputValue * moneyarray[10];
-  }
-  if (amet == 'prügisorteerija') {
-      multiply = inputValue * moneyarray[11];
-  }
-  if (amet == 'pesumasina operaator') {
-      multiply = inputValue * moneyarray[12];
-  }
-  if (amet == 'hooldaja') {
-      multiply = inputValue * moneyarray[13];
-  }
+function getInputValue() {
 
-  document.getElementById("sum").innerHTML = multiply + "€";
+    var moneyarray = [0, 3.27, 3.91, 3.80, 5.58, 3.81, 5.07, 5.00, 7.45, 6.76, 8.20, 6.94, 5.31, 3.99, 6.79, 6.73, 8.22]
+
+    var inputValue = document.getElementById("hours").value;
+    var amet = document.getElementById("occupation").value;
+
+    if (amet == 'Nõudepesija') {
+        multiply = inputValue * moneyarray[1];
+    }
+    if (amet == 'Koristaja') {
+        multiply = inputValue * moneyarray[2];
+    }
+    if (amet == 'Õpetaja') {
+        multiply = inputValue * moneyarray[3];
+    }
+    if (amet == 'Autojuht') {
+        multiply = inputValue * moneyarray[4];
+    }
+    if (amet == 'Lapsehoidja') {
+        multiply = inputValue * moneyarray[5];
+    }
+    if (amet == 'Kokk') {
+        multiply = inputValue * moneyarray[6];
+    }
+    if (amet == 'Elektrik') {
+        multiply = inputValue * moneyarray[7];
+    }
+    if (amet == 'Tehnik') {
+        multiply = inputValue * moneyarray[8];
+    }
+    if (amet == 'Õde') {
+        multiply = inputValue * moneyarray[9];
+    }
+    if (amet == 'Ehitaja') {
+        multiply = inputValue * moneyarray[10];
+    }
+    if (amet == 'Raamatupidaja') {
+        multiply = inputValue * moneyarray[11];
+    }
+    if (amet == 'Õmbleja') {
+        multiply = inputValue * moneyarray[12];
+    }
+    if (amet == 'Juuksur') {
+        multiply = inputValue * moneyarray[13];
+    }
+    if (amet == 'Psüholoog') {
+        multiply = inputValue * moneyarray[13];
+    }
+    if (amet == 'Aednik') {
+        multiply = inputValue * moneyarray[13];
+    }
+    if (amet == 'Logistik') {
+        multiply = inputValue * moneyarray[13];
+    }
+
+    document.getElementById("sum").innerHTML = multiply + "€";
 }
