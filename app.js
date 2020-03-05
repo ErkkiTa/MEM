10739f0bd867555b18e86ac0856698d78e54767e
Karl Gregor Rauniste <karlgregorrauniste@Karls-MacBook-Pro.local>
2020-03-05 10:20:07 +0200

    changes

--- app.js
+++ app.js
@@ -9,163 +9,52 @@ document.querySelectorAll('.feedback li').forEach(entry => entry.addEventListene
 
 //arvutus
 
+function getInputValue() {
 
-function addElement(x1, x2, x3, x4, x5) {
-    var total = x1 + x2 + x3 + x4 + x5;
-    var values = [x1, x2, x3, x4, x5];
-    var offset = 0;
+    var moneyarray = [0, 5.07, 3.80, 5.58, 3.81, 5.78, 3.80, 6.73, 6.04, 5.31, 3.99, 3.77, 4.48, 3.95]
 
-    for (i = 0; i < 5; i++) {
-        var value = (values[i] / total) * 100;
-        var overHalf = 0;
-        var index = i;
+    var inputValue = document.getElementById("hours").value;
+    var amet = document.getElementById("occupation").value;
 
-        if (value > 50) {
-            overHalf = 1;
-        }
-
-
-
-        var style = document.createElement('style');
-        document.head.appendChild(style);
-        style.sheet.insertRule('#slice' + index.toString() + '{--offset: ' + offset.toString() + '; --value: ' +
-            value.toString() + '; --over50: ' + overHalf.toString() + '0; visibility: visible;}');
-        offset += value;
+    if (amet == 'kokk') {
+        multiply = inputValue * moneyarray[1];
     }
-}
-
-function showBottom(e) {
-    console.log(e);
-
-    const el = document.querySelector(e);
-    console.log(el);
-    if (el.classList.contains("input_keys")) {
-        el.classList.remove("input_keys");
-
-    } else {
-        el.classList.add("input_keys");
+    if (amet == 'õpetaja') {
+        multiply = inputValue * moneyarray[2];
     }
+    if (amet == 'autojuht') {
+        multiply = inputValue * moneyarray[3];
+    }
+    if (amet == 'lapsehoidja') {
+        multiply = inputValue * moneyarray[4];
+    }
+    if (amet == 'teenija') {
+        multiply = inputValue * moneyarray[5];
+    }
+    if (amet == 'majahoidja') {
+        multiply = inputValue * moneyarray[6];
+    }
+    if (amet == 'aednik') {
+        multiply = inputValue * moneyarray[7];
+    }
+    if (amet == 'loomakasvataja') {
+        multiply = inputValue * moneyarray[8];
+    }
+    if (amet == 'õmbleja') {
+        multiply = inputValue * moneyarray[9];
+    }
+    if (amet == 'juuksur') {
+        multiply = inputValue * moneyarray[10];
+    }
+    if (amet == 'prügisorteerija') {
+        multiply = inputValue * moneyarray[11];
+    }
+    if (amet == 'pesumasina operaator') {
+        multiply = inputValue * moneyarray[12];
+    }
+    if (amet == 'hooldaja') {
+        multiply = inputValue * moneyarray[13];
+    }
+
+    document.getElementById("sum").innerHTML = multiply + "€";
 }
-// input nrs
-const first = document.querySelector("#first");
-const dishinput = document.querySelector("#dishinput");
-const cleaninput = document.querySelector("#cleaninput");
-const teacherinput = document.querySelector("#teacherinput");
-const driverinput = document.querySelector("#driverinput");
-const sitterinput = document.querySelector("#sitterinput");
-const chefinput = document.querySelector("#chefinput");
-const elecricinput = document.querySelector("#electricinput");
-const technikinput = document.querySelector("#technikinput");
-const nurseinput = document.querySelector("#nurseinput");
-const builderinput = document.querySelector("#builderinput");
-const accountantinput = document.querySelector("#accountantinput");
-const sawinput = document.querySelector("#sawinput");
-const hairinput = document.querySelector("#hairinput");
-const pshyinput = document.querySelector("#pshyinput");
-const gardenerinput = document.querySelector("#gardenerinput");
-const logicinput = document.querySelector("#logicinput");
-// buttons
-const btntulu = document.querySelector("#tulu");
-const dishbtnkulu = document.querySelector("#dishkulu");
-const cleanbtnkuliu = document.querySelector("#cleaninput");
-const teacherbtnkulu = document.querySelector("#teacherinput");
-const driverbtnkulu = document.querySelector("#driverinput");
-const sitterbtnkulu = document.querySelector("#sitterinput");
-const chefbtnkulu = document.querySelector("#chefinput");
-const elecricbtnkulu = document.querySelector("#electricinput");
-const technikbtnkulu = document.querySelector("#technikinput");
-const nursebtnkulu = document.querySelector("#nurseinput");
-const builderbtnkulu = document.querySelector("#builderinput");
-const accountantbtnkulu = document.querySelector("#accountantinput");
-const sawbtnkulu = document.querySelector("#sawinput");
-const hairbtnkulu = document.querySelector("#hairinput");
-const pshybtnkulu = document.querySelector("#pshyinput");
-const gardenerbtnkulu = document.querySelector("#gardenerinput");
-const logicbtnkulu = document.querySelector("#logicinput");
-// totals
-const answer = document.querySelector("#totalanswer");
-const dish = document.querySelector("#dishtotal");
-const clean = document.querySelector("#cleantotal");
-const teacher = document.querySelector("#teachertotal");
-const driver = document.querySelector("#drivertotal");
-const sitter = document.querySelector("#sittertotal");
-const chef = document.querySelector("#chefitotal");
-const electric = document.querySelector("#electrictotal");
-const technik = document.querySelector("#techniktotal");
-const nurse = document.querySelector("#nursetotal");
-const builder = document.querySelector("#buildertotal");
-const accountant = document.querySelector("#accountanttotal");
-const saw = document.querySelector("#sawtotal");
-const hair = document.querySelector("#hairtotal");
-const pshy = document.querySelector("#pshytotal");
-const garndener = document.querySelector("#gardenertotal");
-const logic = document.querySelector("#logictotal");
-// vars
-var currentvalue = 0
-var hometotalvar = 0
-var othertotalvar = 0
-var shoppingtotalvar = 0
-var cartotalvar = 0
-var traveltotalvar = 0
-    // init values
-answer.innerText = currentvalue
-home.innerText = hometotalvar
-other.innerText = othertotalvar
-shopping.innerText = shoppingtotalvar
-car.innerText = cartotalvar
-travel.innerText = traveltotalvar
-
-
-
-function tulu() {
-    currentvalue = currentvalue + parseInt(first.value)
-    answer.innerText = currentvalue;
-}
-
-function homekulu() {
-    currentvalue = currentvalue - parseInt(homeinput.value)
-    answer.innerText = currentvalue;
-    hometotalvar = hometotalvar + parseInt(homeinput.value)
-    home.innerText = hometotalvar;
-    addElement(hometotalvar, shoppingtotalvar, cartotalvar, traveltotalvar, othertotalvar)
-}
-
-function otherkulu() {
-    currentvalue = currentvalue - parseInt(otherinput.value)
-    answer.innerText = currentvalue;
-    othertotalvar = othertotalvar + parseInt(otherinput.value)
-    other.innerText = othertotalvar;
-    addElement(hometotalvar, shoppingtotalvar, cartotalvar, traveltotalvar, othertotalvar)
-}
-
-function shoppingkulu() {
-    currentvalue = currentvalue - parseInt(shoppinginput.value)
-    answer.innerText = currentvalue;
-    shoppingtotalvar = shoppingtotalvar + parseInt(shoppinginput.value)
-    shopping.innerText = shoppingtotalvar;
-    addElement(hometotalvar, shoppingtotalvar, cartotalvar, traveltotalvar, othertotalvar)
-}
-
-function carkulu() {
-    currentvalue = currentvalue - parseInt(carinput.value)
-    answer.innerText = currentvalue;
-    cartotalvar = cartotalvar + parseInt(carinput.value)
-    car.innerText = cartotalvar;
-    addElement(hometotalvar, shoppingtotalvar, cartotalvar, traveltotalvar, othertotalvar)
-}
-
-function travelkulu() {
-    currentvalue = currentvalue - parseInt(travelinput.value)
-    answer.innerText = currentvalue;
-    traveltotalvar = traveltotalvar + parseInt(travelinput.value)
-    travel.innerText = traveltotalvar;
-    addElement(hometotalvar, shoppingtotalvar, cartotalvar, traveltotalvar, othertotalvar)
-}
-
-// 3. Add Event listener to button
-btntulu.addEventListener("click", tulu);
-homebtnkulu.addEventListener("click", homekulu);
-otherbtnkulu.addEventListener("click", otherkulu);
-shoppingbtnkulu.addEventListener("click", shoppingkulu);
-carbtnkulu.addEventListener("click", carkulu)
-travelbtnkulu.addEventListener("click", travelkulu);
